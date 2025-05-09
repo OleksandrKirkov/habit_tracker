d39e66197f2b8eea92daa09f23432880a81886f0
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-05-09 20:47:13 +0300

    feat: create sync backup controller

---
+++ Controllers/SyncBackupController.cs
@@ -0,0 +1,32 @@
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SyncBackupController : ControllerBase
+    {
+        private readonly ISyncBackupService _backups;
+
+        public SyncBackupController(ISyncBackupService backups)
+        {
+            _backups = backups;
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetByUser(Guid userId)
+        {
+            var list = await _backups.GetByUserAsync(userId);
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] SyncBackup model)
+        {
+            var created = await _backups.CreateAsync(model);
+            return Ok(created);
+        }
+    }
+}

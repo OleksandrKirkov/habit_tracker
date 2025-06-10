1328820fc54fda44f5997b5b8dbf02e85740214a
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-11 01:10:46 +0300

    feat: create Habits DTO

---
+++ Core/DTO/Habits.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using Core.Models;
+
+namespace Core.DTO.Habits
+{
+    public class CreateHabitRequest
+    {
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(1)]
+        public string Color { get; set; } = null!;
+
+        [Required]
+        [MinLength(1)]
+        public string Icon { get; set; } = null!;
+
+        [Range(1, 7)]
+        public short Frequency { get; set; }
+
+        [EnumDataType(typeof(HabitType))]
+        public string Type { get; set; } = null!;
+
+        public TimeSpan? ReminderTime { get; set; }
+
+        [EnumDataType(typeof(ReminderMode))]
+        public string ReminderMode { get; set; } = null!;
+    }
+
+    public class GetHabitsByUser
+    {
+        public List<Habit> Habits { get; set; } = new();
+    }
+}

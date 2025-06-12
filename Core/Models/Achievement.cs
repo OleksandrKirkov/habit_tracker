e3979670260f0b5b0aea5813bc401cf825e22fe2
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-12 07:49:58 +0300

    feat: update id field and other fields in achievement model

--- Core/Models/Achievement.cs
+++ Core/Models/Achievement.cs
@@ -1,21 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Core.Models
 {
     public class Achievement
     {
         [Key]
-        public Guid Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
         [Required]
-        public string Code { get; set; }
+        public string Code { get; set; } = null!;
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = null!;
 
-        public string Icon { get; set; }
+        public string Icon { get; set; } = null!;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 

2ba61e1e765c93479b6cd73213e28bce6b4c643d
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-06-12 07:50:48 +0300

    feat: update id field in integration model

--- Core/Models/Integraion.cs
+++ Core/Models/Integraion.cs
@@ -6,20 +6,21 @@ namespace Core.Models
     public class Integration
     {
         [Key]
-        public Guid Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
         [Required]
-        public Guid UserId { get; set; }
+        public int UserId { get; set; }
 
         [ForeignKey(nameof(UserId))]
-        public User User { get; set; }
+        public User User { get; set; } = null!;
 
         [Required]
-        public string Provider { get; set; }
+        public string Provider { get; set; } = null!;
 
-        public string AccessToken { get; set; }
+        public string AccessToken { get; set; } = null!;
 
-        public string RefreshToken { get; set; }
+        public string RefreshToken { get; set; } = null!;
 
         public DateTime Linkedat { get; set; } = DateTime.UtcNow;
     }
